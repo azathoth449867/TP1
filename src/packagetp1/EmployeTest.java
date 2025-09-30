981469e79732a2c9a414196a793240cd994ce96a
azathoth449867 <azathoth0527689@gmail.com>
2025-09-30 14:27:47 -0400

    test

---
+++ src/packagetp1/EmployeTest.java
@@ -0,0 +1,39 @@
+package packagetp1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        Employe emp = new Employe()
+    }
+
+    @org.junit.jupiter.api.Test
+    void salaireBrut() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void salaireNetAvantImpot() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void impotFederal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void impotProvincial() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void salaireNetApresImpot() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void joursVacances() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void heuresSup() {
+    }
+}
