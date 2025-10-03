540923b6abb78ea02f79564ff1186add9461fd92
azathoth449867 <azathoth0527689@gmail.com>
2025-10-03 19:10:54 -0400

    Fini

--- src/packagetp1/EmployeTest.java
+++ src/packagetp1/EmployeTest.java
@@ -18,27 +18,32 @@ class EmployeTest {
 
     @org.junit.jupiter.api.Test
     void salaireNetAvantImpot() {
-        assertEquals(332.96128, emp.salaireNetAvantImpot());
+        assertEquals(332.96, emp.salaireNetAvantImpot(), 0.009);
     }
 
     @org.junit.jupiter.api.Test
     void impotFederal() {
-        assertEquals(63.2626432, emp.impotFederal());
+        assertEquals(63.26, emp.impotFederal(), 0.009);
     }
 
     @org.junit.jupiter.api.Test
     void impotProvincial() {
+        assertEquals(66.59,emp.impotProvincial(), 0.009);
     }
 
     @org.junit.jupiter.api.Test
     void salaireNetApresImpot() {
+        assertEquals(203.10,emp.salaireNetApresImpot(), 0.009);
     }
 
     @org.junit.jupiter.api.Test
     void joursVacances() {
+        assertEquals(19, emp.joursVacances());
     }
 
     @org.junit.jupiter.api.Test
     void heuresSup() {
+        emp.heuresSup(-60);
+        assertEquals(32,emp.getNbreHeuresSemaine() );
     }
 }
