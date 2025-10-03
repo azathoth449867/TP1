427ab37bd5b0e74b5196c8092061830561aadfe2
azathoth449867 <azathoth0527689@gmail.com>
2025-10-02 22:45:11 -0400

    3 test de fait

--- src/packagetp1/EmployeTest.java
+++ src/packagetp1/EmployeTest.java
@@ -4,21 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeTest {
 
+    Employe emp;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-        Employe emp = new Employe()
+        emp = new Employe("Sheldon Cooper","323",12.60, 32, 4 );
     }
 
     @org.junit.jupiter.api.Test
     void salaireBrut() {
+        assertEquals(403.20, emp.salaireBrut());
     }
 
     @org.junit.jupiter.api.Test
     void salaireNetAvantImpot() {
+        assertEquals(332.96128, emp.salaireNetAvantImpot());
     }
 
     @org.junit.jupiter.api.Test
     void impotFederal() {
+        assertEquals(63.2626432, emp.impotFederal());
     }
 
     @org.junit.jupiter.api.Test
